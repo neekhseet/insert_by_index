ba3c7b77b634901854d8acc1b520b05cf398b5af
alex <iteeshkeen@gmail.com>
2026-03-16 16:54:51 +0500

    insert number algorithm

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define TOTAL_DIGITS    14
 
 int main(void) {
-    printf("Hello, World!\n");
+    int arr[TOTAL_DIGITS] = { 3, 4, 5, 6 };
+    int insert_indx = 2;
+
+    for (int i = TOTAL_DIGITS - 1; i > insert_indx; i--) {
+        arr[i] = arr[i-1];
+    }
+
+    arr[insert_indx] = 10;
+
+    for (int i = 0; i < TOTAL_DIGITS; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
